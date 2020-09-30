8838bfeded1c9a13c871f8163809afcbe11744be
Alejandro <rodrigueznatalandy@gmail.com>
2020-09-30 00:12:07 -0400

    Added a List View for displaying possible filenames in the future

--- GUI/VanGogh/app/src/main/java/com/example/vangogh/FileManager.java
+++ GUI/VanGogh/app/src/main/java/com/example/vangogh/FileManager.java
@@ -6,28 +6,47 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.FileUtils;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class FileManager extends Activity {
     private static final int FILE_SELECTED_CODE = 0;
     private static final String TAG = "FILE MANAGER ACTIVITY";
     private static final int REQUEST_CHOOSER = 1234;
 
+    private FileArrayAdapter files_adapter;
+    private ArrayList<String> files = new ArrayList<>();
+
+    private void testFilenames()
+    {
+        files.add("A");
+        files.add("AB");
+        files.add("B");
+        files.add("O");
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_manager);
 
+        testFilenames();
+        files_adapter = new FileArrayAdapter(this, files);
+        ListView list_view = (ListView) findViewById(R.id.files_list) ;
+        list_view.setAdapter(files_adapter);
         //Create the ACTION_GET_CONTENT Intent
 //        Intent getContentIntent = FileUtils.createGetContentIntent();
 
 //        Intent intent = Intent.createChooser(getContentIntent, "Select a file");
 //        startActivityForResult(intent, REQUEST_CHOOSER);
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        startActivityForResult(intent, REQUEST_CHOOSER);
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//        startActivityForResult(intent, REQUEST_CHOOSER);
     }
 
 
