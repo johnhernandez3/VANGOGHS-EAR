4bc68d62c225593e5c22fb6acd74d20eba6f6c71
Alejandro <rodrigueznatalandy@gmail.com>
2020-10-27 14:52:08 -0400

    Added new record button image

--- GUI/VanGogh/app/src/main/java/chords/Tablature.java
+++ GUI/VanGogh/app/src/main/java/chords/Tablature.java
@@ -1,21 +1,48 @@
 package chords;
 
+import java.util.ArrayList;
+
 public class Tablature
 {
     private final String[] GUITAR_STRINGS =  {"E", "A", "D", "G", "B","E"};
+    private ArrayList<String> chords;
+    private ArrayList<Integer> frets;
 
     public Tablature()
     {
-
+        prepareFrets();
     }
 
     @Override
     public String toString()
     {
         //TODO: String representation of a Tablature
-        String repr = "";
+//        String repr = "";
+        StringBuilder str_builder = new StringBuilder();
 
-        return repr;
+        for(String g_string: GUITAR_STRINGS)
+        {
+            str_builder.append(g_string);
+            for(int i : frets)
+            {
+                str_builder.append("-");
+                str_builder.append("|");
+            }
+            str_builder.append("-");
+            str_builder.append("\n");
+        }
+
+        return str_builder.toString();
+    }
+
+    private void prepareFrets()
+    {
+        frets = new ArrayList<>();
+        //TODO: May be wrong here on the frets existing on a guitar...
+        for(int i=1; i < 13; i++)
+        {
+            frets.add(i);
+        }
     }
 
 
