c61511ad111acb77b3228b60846d39ba38eb260d
Alejandro <rodrigueznatalandy@gmail.com>
2020-11-02 20:05:15 -0400

    There is an error with opening files and the Tensors

--- Music_Analysis/ML/music_classifier.py
+++ Music_Analysis/ML/music_classifier.py
@@ -20,7 +20,7 @@ from platform import system
 import inspect
 
 
-
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 ROOT_DIR = None
 
@@ -70,8 +70,10 @@ def chord_files(path=chords_paths(), chord_name='a'):
     # for chord_audio_path in findPattern(path, pattern=chord_name):
     #     yield chord_audio_path
     for chord in Path.iterdir(path):
+        print(chord)
         if chord.is_file():
-            yield chord
+            print(chord.name)
+            yield chord.name
 
 def  decode_audio(audio_binary):
     audio, _  = tf.audio.decode_wav(audio_binary)
@@ -86,22 +88,33 @@ def extract_chord_class(chord_filename:str)->str:
     return None
 
 def get_label(file_path):
-    '''
-        TODO: Doing too many things in this method, refactor to only do a single operation'''
-    
-    # if file_path.is_dir():
-    #     for chord_audio in Path.iterdir(file_path):
-    #         if chord_audio.is_file():
-    #             yield extract_chord_class(PurePath(chord_audio).name)
+  file_path =tf.cast(file_path, tf.string)
 
-    # if file_path.is_file():
-    #             return extract_chord_class(PurePath(file_path).name)
-    if file_path.is_file() or file_path.is_dir():
-        return extract_chord_class(PurePath(file_path).name)
-    else:
-        raise ValueError(f"Invalid file path provided:{file_path}")
+  parts = tf.strings.split(file_path, os.path.sep)
+
+  # Note: You'll use indexing here instead of tuple unpacking to enable this 
+  # to work in a TensorFlow graph.
+  return parts[-2] 
+
+# def get_label(file_path):
+#     '''
+#         TODO: Doing too many things in this method, refactor to only do a single operation'''
+    
+#     # if file_path.is_dir():
+#     #     for chord_audio in Path.iterdir(file_path):
+#     #         if chord_audio.is_file():
+#     #             yield extract_chord_class(PurePath(chord_audio).name)
+
+#     # if file_path.is_file():
+#     #             return extract_chord_class(PurePath(file_path).name)
+#     if file_path.is_file() or file_path.is_dir():
+#         return extract_chord_class(PurePath(file_path).name)
+#     else:
+#         raise ValueError(f"Invalid file path provided:{file_path}")
 
 def get_waveform_and_label(file_path):
+    print(type(file_path))
+    print(file_path)
     label = get_label(file_path)
     audio_binary = tf.io.read_file(file_path)
     waveform = decode_audio(audio_binary)
@@ -130,6 +143,14 @@ def preprocess():
     waveforms_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
     return waveforms_ds
 
+def preprocess_dataset(files):
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    files_ds = [chord_file for chord_file in files]
+    files_ds = tf.data.Dataset.from_tensor_slices(files_ds)
+    waveforms_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
+    spectrogram_ds = waveforms_ds.map(get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE )
+    return spectrogram_ds
+
 def get_label_from_slice(waveform_ds):
 
     for label, wv in waveform_ds:
@@ -143,13 +164,23 @@ def waveform_to_spectrogram_ds(waveform_ds):
         spect = get_spectrogram(wv)
         yield label, spect
 
-def get_spectrogram_and_label_id(audio, label, num_labels):
+def get_spectrogram_and_label_id(audio, label, num_labels=10):
     spectrogram = get_spectrogram(audio)
     spectrogram = tf.expand_dims(spectrogram, -1)
     label_id = tf.argmax(label == num_labels)
     return spectrogram, label_id
 
-def chord_classifier_model(input_shape, norm_layer, num_labels=10):
+def divide_ds():
+    fs = [chord_files()]
+
+    if len(fs) % 3 == 0:
+        train, val, test = fs[0:len(fs)//3 - 1],fs[len(fs)//3: 2 * len(fs)//3 -1], fs[ 2 * len(fs)//3 : len(fs) -1]
+    else:
+        size = len(fs)
+        train, val, test = fs[0: size//3 - 1],fs[size//3: 2 * size//3 -1], fs[ 2 * size //3 ::]
+    return train, val, test
+
+def chord_classifier_model(input_shape, norm_layer=None, num_labels=10):
     norm_layer = preprocessing.Normalization()
     norm_layer.adapt(spectrogram_dataset().map(lambda x,_: x))
     model = models.Sequential(
@@ -176,15 +207,46 @@ def chord_classifier_model(input_shape, norm_layer, num_labels=10):
         metrics=['accuracy'],
     )
     return model
+
+def init_training():
+    train, val, test = divide_ds()
+
+    # train_ds = train.map(get_waveform_and_label, num_calls=AUTOTUNE)
+    # train_ds = train_ds.map(get_spectrogram_and_label_id,num_calls=AUTOTUNE)
+    train_ds = preprocess_dataset(train)
+    val_ds = preprocess_dataset(val)
+    test_ds = preprocess_dataset(test)
+
+    model = chord_classifier_model(input_shape=train_ds.shape)
+    hist = train_model(train_ds, val_ds,model, )
+
+    plot_model_loss(hist)
+
+def train_model(train_dataset, validation_data, model, epochs=10):
+    batch_size = 64
+
+    train_dataset = train_dataset.batch(batch_size)
+    validation_data = validation_data.batch(batch_size)
     
-def train(train_dataset, validation_data, model, epochs=10):
+    train_dataset = train_dataset.cache().prefetch(AUTOTUNE)
+    validation_data = validation_data.cache().prefetch(AUTOTUNE)
+    
+
     history = model.fit(
         train_dataset,
         validation_data=validation_data,
         epochs=epochs,
         callbacks=tf.keras.callbacks.EarlyStopping(verbose=1,patience=2),
     )
-    return
+    return history
+
+def plot_model_loss(history):
+    metrics = history.history
+    plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+    plt.legend(['loss', 'val_loss'])
+    plt.show()
+
+
 
 def confusion_matrix(model, labels,y_true,y_pred):
 
@@ -194,3 +256,10 @@ def confusion_matrix(model, labels,y_true,y_pred):
     plt.xlabel('Prediction')
     plt.ylabel('Label')
     plt.show()
+
+def main():
+    init_training()
+    return
+
+if __name__ == "__main__":
+    main()
