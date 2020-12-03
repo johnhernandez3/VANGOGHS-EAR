6c0afed38f551f4e3dcf4d34232ebf19aaa9d2bf
Alejandro <rodrigueznatalandy@gmail.com>
2020-12-03 12:36:37 -0400

    Added WAV fixes and Classifier changes into Mic

--- GUI/VanGogh/app/src/main/java/io_devices/Microphone.java
+++ GUI/VanGogh/app/src/main/java/io_devices/Microphone.java
@@ -9,17 +9,36 @@ import android.os.SystemClock;
 import android.util.Log;
 
 import com.jlibrosa.audio.JLibrosa;
+import com.jlibrosa.audio.wavFile.WavFileException;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import utils.Device;
+import utils.MapEntryComparator;
 
 /**
  * Class for representing the IO Device of a Microphone for the AudioRecorder class.
@@ -365,80 +384,220 @@ public class Microphone implements Device
     private void classifyRecording(String fileName, Context context)
     {
 
-//        Runnable loadRunnable =  new  Runnable {
-//            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-//            // Creates a toast pop-up.
-//            // This is to know if this runnable is running on UI thread or not!
-//            try {
-//                SharedPreferences sharedPreferences = null;
-//
-//
-//                sharedPreferences = context.getSharedPreferences(GENRE_PREFERENCES, Context.MODE_PRIVATE);
-//
-//
-//                String audioFilePath = fileName;
-//
-//                try {
-//
-//                    int defaultSampleRate =  -1; //-1 value implies the method to use default sample rate
-//
-//                    int defaultAudioDuration = -1 ; //-1 value implies the method to process complete audio duration
-//
-//
-//                    JLibrosa jLibrosa = new JLibrosa();
-//                    //val audioFeatureValues: FloatArray = jLibrosa.loadAndRead(audioFilePath, defaultSampleRate, defaultAudioDuration)
-//                    ArrayList<Float> audioFeatureValuesList = jLibrosa.loadAndReadAsList(
-//                            audioFilePath,
-//                            defaultSampleRate,
-//                            defaultAudioDuration
-//                    );
-//
-//
-//                    var splitSongs: ArrayList<List<Float>> =
-//                    RecordingRepository.splitSongs(audioFeatureValuesList, 0.5)
-//
-//                    var predictionList : ArrayList<String?> = ArrayList<String?>()
-//                    for(i in 0 until splitSongs.size){
-//                        var audioArr: FloatArray = splitSongs[i].toFloatArray()
-//
-//                        var melSpectrogram: Array<FloatArray> =
-//                        jLibrosa.generateMelSpectroGram(audioArr, 22050, 1024, 128, 256)
-//
-//
-//                        var prediction : String? = loadModelAndMakePredictions(melSpectrogram, context);
-//                        predictionList.add(prediction)
-//                        println("test")
-//                    }
-//
+        Runnable loadRunnable =  new  Runnable( {
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            // Creates a toast pop-up.
+            // This is to know if this runnable is running on UI thread or not!
+            try {
+                SharedPreferences sharedPreferences = null;
+
+
+                sharedPreferences = context.getSharedPreferences(GENRE_PREFERENCES, Context.MODE_PRIVATE);
+
+
+                String audioFilePath = fileName;
+
+                try {
+
+                    int defaultSampleRate =  -1; //-1 value implies the method to use default sample rate
+
+                    int defaultAudioDuration = -1 ; //-1 value implies the method to process complete audio duration
+
+
+                    JLibrosa jLibrosa = new JLibrosa();
+                    float audioFeatureValues[] = jLibrosa.loadAndRead(audioFilePath, defaultSampleRate, defaultAudioDuration)
+                    ArrayList<Float> audioFeatureValuesList = jLibrosa.loadAndReadAsList(
+                            audioFilePath,
+                            defaultSampleRate,
+                            defaultAudioDuration
+                    );
+
+
+                    ArrayList<List<Float>> splitSongs  = splitSongs(audioFeatureValuesList, 0.5);
+
+                    ArrayList<String> predictionList  = new ArrayList<String>();
+                    for(int i=0; i <  splitSongs.size(); i++){
+                        float audioArr [] =(float[]) splitSongs[i].toArray();
+
+                        float melSpectrogram[][]=
+                        jLibrosa.generateMelSpectroGram(audioArr, 22050, 1024, 128, 256)
+
+
+                        String prediction  = loadModelAndMakePredictions(melSpectrogram, context);
+                        predictionList.add(prediction);
+//                        println("test");
+                    }
+
 //                    println(predictionList.groupingBy { it }.eachCount().filter { it.value > 1 })
+
+//                    List predList = predictionList.groupingBy { it }.eachCount();
+                    Map predList = predictionList.stream().collect(Collectors.toMap(Function.identity(), e-> 1,Math::addExact));
+//                    List sortedPredList = predList.entrySet().sortedByDescending { it.value }.associate { it.toPair()};
+                    String sortedPredList = predList.entrySet().stream().sorted(new MapEntryComparator()).iterator().next();
+//                    String predValue = sortedPredList.entrySet().iterator().next();
 //
-//                    val predList = predictionList.groupingBy { it }.eachCount()
-//                    val sortedPredList = predList.entries.sortedByDescending { it.value }.associate { it.toPair()}
-//
-//                    val predValue: String? = sortedPredList.iterator().next().key
-//
-//                    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+//                     SharedPreferences.Editor  editor = sharedPreferences.edit();
 //                    editor.putString(fileName, predValue);
-//                    editor.commit()
-//
-//
-//                }
-//                catch(e: WavFileException){
-//                    println(e.message)
-//                }
-//
-//
-//            } catch (ex: Exception) {
-//                println(ex.message)
-//            }
-//        }
-//        val predictionThread = Thread(loadRunnable);
-//        predictionThread.start();
-//
+//                    editor.commit();
 
 
+                }
+                catch(WavFileException e){
+                    e.printStackTrace();
+                   Log.e(TAG, e.getMessage());
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread predictionThread = new  Thread(loadRunnable);
+        predictionThread.start();
 
     }
+
+    protected String loadModelAndMakePredictions(float meanMFCCValues[][] , Context context) throws IOException {
+
+
+
+        String predictedResult = "unknown";
+
+
+        Interpreter tflite;
+
+    //load the TFLite model in 'MappedByteBuffer' format using TF Interpreter
+        MappedByteBuffer tfliteModel  =  FileUtil.loadMappedFile(context, getModelPath());
+    /** Options for configuring the Interpreter.  */
+    Interpreter.Options tfliteOptions = new  Interpreter.Options();
+    tfliteOptions.setNumThreads(2);
+    tflite = new Interpreter(tfliteModel, tfliteOptions);
+
+    //get the datatype and shape of the input tensor to be fed to tflite model
+    int imageTensorIndex = 0;
+
+    DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+
+    int imageDataShape[] = tflite.getInputTensor(imageTensorIndex).shape();
+
+    //get the datatype and shape of the output prediction tensor from tflite model
+    int probabilityTensorIndex = 0;
+    int[] probabilityShape =
+            tflite.getOutputTensor(probabilityTensorIndex).shape();
+    DataType probabilityDataType =
+            tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+
+
+        //The for at the end is the amount of bytes that a float occupies in Java
+        // taken from the mfcc mean array
+        ByteBuffer byteBuffer  = ByteBuffer.allocate(4*meanMFCCValues.length* meanMFCCValues[0].length);
+
+    for(int i= 0;i <  meanMFCCValues.length; i++){
+        float valArray[] = meanMFCCValues[i];
+        int inpShapeDim[] = {1,1,meanMFCCValues[0].length,1};
+        TensorBuffer valInTnsrBuffer = TensorBuffer.createDynamic(imageDataType);
+        valInTnsrBuffer.loadArray(valArray, inpShapeDim);
+        ByteBuffer  valInBuffer = valInTnsrBuffer.getBuffer();
+        byteBuffer.put(valInBuffer);
+    }
+
+    byteBuffer.rewind();
+
+    //val inpBuffer: ByteBuffer? = convertBitmapToByteBuffer(bitmp)
+    TensorBuffer outputTensorBuffer =
+            TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+    //run the predictions with input and output buffer tensors to get probability values across the labels
+    tflite.run(byteBuffer, outputTensorBuffer.getBuffer());
+
+
+    //Code to transform the probability predictions into label values
+    String ASSOCIATED_AXIS_LABELS = "labels.txt";
+        List<String> associatedAxisLabels  = null ;
+    try {
+        associatedAxisLabels = FileUtil.loadLabels(context, ASSOCIATED_AXIS_LABELS)
+    } catch ( IOException e) {
+        Log.e("tfliteSupport", "Error reading label file", e);
+    }
+
+    //Tensor processor for processing the probability values and to sort them based on the descending order of probabilities
+        TensorProcessor probabilityProcessor = new TensorProcessor.Builder().add(new NormalizeOp(0.0f, 255.0f)).build();
+    if (null != associatedAxisLabels) {
+        // Map of labels and their corresponding probability
+        TensorLabel labels = new TensorLabel(
+                associatedAxisLabels,
+                probabilityProcessor.process(outputTensorBuffer)
+        );
+
+        // Create a map to access the result based on label
+        Map<String, Float> floatMap = labels.getMapWithFloatValue();
+
+        //function to retrieve the top K probability values, in this case 'k' value is 1.
+        //retrieved values are storied in 'Recognition' object with label details.
+        List<Recognition> resultPrediction  = getTopKProbability(floatMap);
+
+        //get the top 1 prediction from the retrieved list of top predictions
+        predictedResult = getPredictedValue(resultPrediction);
+
+    }
+    return predictedResult;
+
+}
+
+    private String getModelPath() {
+        String res = "";
+        return res;
+    }
+    public String  getPredictedValue(List<Recognition> predictedList) {
+        Recognition top1PredictedValue  = predictedList.get(0);
+    return top1PredictedValue.getTitle();
+}
+
+
+
+    /** Gets the top-k results.  */
+    protected List<Recognition> getTopKProbability(Map<String, Float> labelProb ) {
+    // Find the best classifications.
+    int MAX_RESULTS = 1;
+    PriorityQueue<Recognition>  pq = new PriorityQueue(
+            MAX_RESULTS,
+            Comparator<Recognition> { lhs, rhs -> // Intentionally reversed to put high confidence at the head of the queue.
+                    java.lang.Float.compare(rhs.getConfidence(), lhs.getConfidence())
+            })
+    for (Map.Entry<String, Float> entry : labelProb) {
+        pq.add(Recognition("" + entry.getKey(), entry.getKey(), entry.getValue()));
+    }
+
+        ArrayList<Recognition> recognitions = new ArrayList();
+    int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+    for (int i = 0; i < recognitionsSize ; i++) {
+        recognitions.add(pq.poll());
+    }
+    return recognitions
+}
+
+
+
+    private ArrayList<List<Float>> splitSongs(ArrayList<Float> audioFeatureValuesList, double overlap) {
+        int chunk = 3300;
+        int offset =( int) (chunk * (1 - overlap)) ;
+        int x_shape = audioFeatureValuesList.size();
+
+        float x_max_index = x_shape / chunk;
+
+        float x_max = (x_max_index * chunk) - chunk;
+
+        ArrayList<List<Float>>splitSongValList = new ArrayList<List<Float>>();
+
+        for( int i = 0; i <  x_max + 1; i+=offset){
+            splitSongValList.add(audioFeatureValuesList.subList(i, i+chunk));
+        }
+
+        return splitSongValList;
+    }
+
+
 
     /**
      * Initiates the process of storing data from the microphone into the internal file
